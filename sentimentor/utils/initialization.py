3fd1478570788daa05c8640e2190cf67d533e176
bobchien <bob.sc.chien@gmail.com>
2021-11-21 16:42:35 +0800

    add setup functions for colab computation resources

--- sentimentor/utils/initialization.py
+++ sentimentor/utils/initialization.py
@@ -72,11 +72,39 @@ np.set_printoptions(suppress=True)
 
 RESERVED_TOKENS = ["[PAD]", "[UNK]", "[START]", "[END]"]
 
-### GPU
+##### Setup computation resources
 
-physical_devices = tf.config.list_physical_devices('GPU') 
-try:
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+### CPU / GPU /TPU
+
+try: 
+    # TPU Detection
+    cluster_resolver = tf.distribute.cluster_resolver.TPUClusterResolver()
+    # Connect to the TPUs
+    tf.config.experimental_connect_to_cluster(cluster_resolver)
+    # This is the TPU initialization code that has to be at the beginning.
+    tf.tpu.experimental.initialize_tpu_system(cluster_resolver)
+
+    strategy = tf.distribute.TPUStrategy(cluster_resolver)
+    print('\nRunning on TPU ', cluster_resolver.cluster_spec().as_dict()['worker'])
+    print("All devices:", tf.config.list_logical_devices('TPU'))
 except:
-    # Invalid device or cannot modify virtual devices once initialized.
-    pass
+    physical_devices = tf.config.list_physical_devices('GPU') 
+    if physical_devices:
+        tf.config.experimental.set_memory_growth(physical_devices[0], True)
+        strategy = tf.distribute.MirroredStrategy()
+        print('\nRunning on GPU...')
+    else:
+        strategy = tf.distribute.get_strategy()
+        print('\nRunning on CPU...')
+        
+### RAM     
+
+from psutil import virtual_memory
+
+RAM_GB = virtual_memory().total / 1e9
+print('\nYour runtime has {:.1f} gigabytes of available RAM\n'.format(RAM_GB))
+
+if RAM_GB < 20:
+    print('Not using a high-RAM runtime')
+else:
+    print('You are using a high-RAM runtime!')
